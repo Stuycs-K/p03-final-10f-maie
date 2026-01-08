24d477a015a921811d89f0ed0c49c3aba9f263b5
Emily Mai <emilym384@nycstudents.net>
2026-01-08 15:23:47 -0500

    basic client and server sending messages

--- client.c
+++ client.c
@@ -1,7 +1,35 @@
 #include "networking.h"
 
 void clientLogic(int server_socket){
-  
+  while (1) {
+    //prompts the user for a string
+    printf("client, enter a message: ");
+    char input[BUFFER_SIZE];
+    if (fgets(input, BUFFER_SIZE, stdin) != NULL) {
+      int len = strlen(input);
+      if (len > 0 && input[len-1] == '\n') {
+        input[len -1] = '\0'; //remove new line
+      }
+      //send the user input to the server
+      int bytes_written = write(server_socket, input, sizeof(input));
+      err(bytes_written, "write error");
+    }
+    else {
+      printf("Client closed\n");
+      break;
+    }
+    //read message from the server
+    char buffer[BUFFER_SIZE];
+    int bytes_read = read(server_socket, buffer, sizeof(buffer));
+    if (bytes_read == 0) {
+      printf("Socket closed\n");
+      break;
+    }
+    err(bytes_read, "read error");
+
+    //prints the modified string
+    //printf("client received: '%s'\n", buffer);
+  }
 }
 
 int main(int argc, char *argv[] ) {
--- server.c
+++ server.c
@@ -2,11 +2,26 @@
 
 
 void subserver_logic(int client_socket){
- 
+  while (1) {
+    //listens for a string (use the buffer size)
+    char buffer[BUFFER_SIZE];
+    int bytes_read = read(client_socket, buffer, sizeof(buffer));
+    if (bytes_read == 0) {
+      printf("Socket closed\n");
+      break;
+    }
+    err(bytes_read, "read error");
+    printf("server received: %s\n", buffer);
+
+    //server writes back message to client
+    int bytes_written = write(client_socket, buffer, bytes_read);
+    err(bytes_written, "write error");
+    //printf("server sending back: %s\n", buffer);
+  }
 }
 
-int main(int argc, char *argv[] ) { 
-  int listen_socket = server_setup(); 
+int main(int argc, char *argv[] ) {
+  int listen_socket = server_setup();
   printf("bind complete\n");
   printf("server listening for connections.\n");
   while (1) {
